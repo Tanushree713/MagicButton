71e332f0786cdc62262498c07b30ef28dcfff37d
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2023-04-25 15:27:45 +0530

    Seventeenth commit

---
+++ server.js
@@ -0,0 +1,14 @@
+function header(){
+    const header = document.getElementById("header");
+    header.style.display = "block";
+}
+function section(){
+     const section = document.getElementById("section");
+     section.style.display = "block";
+}
+function footer(){
+    const footer = document.getElementById("footer");
+    footer.style.display = "block";
+}
+
+  
